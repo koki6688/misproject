9b9f26891505243a4c326145fe3913fc34cffa11
mis104project <32444468+mis104project@users.noreply.github.com>
2017-11-06 11:43:21 +0800

    controllers edited

--- controllers/task.js
+++ controllers/task.js
@@ -11,3 +11,22 @@ exports.task = function (req, res) {
 
     //獲取輸入內容
 
+    var category = req.body.category;
+    var duetime = new Date().toLocaleString();
+    var content = req.body.content;
+    var level = req.body.level;
+};
+
+
+//存至DB
+
+TaskModel.addTask({
+    title: title, category: category, content: content,
+    chat: chat, level: level
+}, function (err, result) {
+    if (err) {
+        ep.emit('info_error', 'error')
+    }else
+        res.render('new-task')
+}
+);
--- routes/web_routers.js
+++ routes/web_routers.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var signController = require('../controllers/register');
 var postController = require('../controllers/post_task');
 var acceptController = require('../controllers/accept_task');
+var taskController = require('../Controllers/task')
 
 
 router.get('/', function (req, res) {
@@ -15,6 +16,12 @@ router.get('/register', signController.showRegister);
 
 /* 提交註冊訊息 */
 router.post('/register', signController.register);
+
+/* 顯示task頁面 */
+router.get('/task', taskController.showTask);
+
+/* 提交task訊息 */
+router.post('/task', taskController.task);
 
 /* 貼出任務 */
 router.post('/post', postController.post);
