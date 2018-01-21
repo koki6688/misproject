da1da970a3bd814dc5fd4d25ab86153c9e2dce79
joe <speechlesslul@gmail.com>
2018-01-21 12:50:44 +0800

    openshift try

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL + 'process.env.OPENSHIFT_APP_NAME' , {useMongoClient: true});
+mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME , {useMongoClient: true});
 
 exports.mongoose = mongoose;
