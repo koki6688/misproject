e0440c42ea33543409a8908bff94a1cfdaed98a4
joe <speechlesslul@gmail.com>
2018-01-21 13:30:58 +0800

    openshift try ip host

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var url ='mongodb://127.0.0.1:27017/misproject';
-if(process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME){
+if(process.env.OPENSHIFT_MONGODB_DB_URL){
     url = process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME;
 }
 mongoose.Promise = global.Promise;
